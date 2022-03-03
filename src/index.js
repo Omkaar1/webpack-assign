b15e13eb0d62472610371c9a5915a9533f505dbf
Omkar Londhe <imomkarlondhe@gmail.com>
2022-03-03 21:29:18 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import "./index.css";
+import "./file.png";
+
+const note = JSON.parse(localStorage.getItem("todonote")) || [];
+const button = document.getElementById("note_btn");
+
+button.addEventListener("click", addNote);
+function addNote(e) {
+  e.preventDefault();
+  const input = document.getElementById("text").value;
+  //console.log(input);
+  const todo = {
+    todoNote: input,
+  };
+  note.push(todo);
+  localStorage.setItem("todonote", JSON.stringify(note));
+  appendnote(note);
+}
+
+function appendnote(note) {
+  note.map(function (item, index) {
+    var container = document.getElementById("container");
+
+    var smallnote = document.createElement("div");
+    smallnote.setAttribute("id", "smalldiv");
+    smallnote.textContent = item.todoNote;
+
+    var remove = document.createElement("button");
+    remove.innerHTML = "<button>Remove</button>";
+
+    // remove.addEventListener(click,function(){
+    //     Delete(index);
+    // })
+
+    container.append(smallnote);
+  });
+}
+
+window.addEventListener("load", function () {
+  var note = JSON.parse(localStorage.getItem("todonote")) || [];
+  appendnote(note);
+});
